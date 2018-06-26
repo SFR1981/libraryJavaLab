fc2da81012cf293a060ce54d6ecbe057c230b3b8
Stephen Rooney <s.f.rooney@gmail.com>
2018-06-26 15:22:02 +0100

    book count added

---
+++ src/main/java/Library.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private int capacity;
+    private ArrayList<Book> books;
+
+    public Library(){
+        this.capacity = 100;
+        this.books = new ArrayList<>();
+    }
+
+    public int getBooks() {
+        return books.size();
+    }
+}
